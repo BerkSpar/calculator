a0e157c5c4ec4a07de76f8af858f0f1a80445ae6
Felipe Passos <felipe@lftecnologia.com.br>
2022-09-04 18:17:02 -0300

    first commit

--- aula2/ViewController.swift
+++ aula2/ViewController.swift
@@ -7,13 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension String {
+    var floatValue: Float {
+        let result = self.replacingOccurrences(of: ",", with: ".")
+        return (result as NSString).floatValue
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
+    var currentValue: String = ""
+    var resultValue: Float = 0.0
+    var operatorValue: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabel()
     }
 
-
+    @IBAction func onTapNumber(_ sender: UIButton) {
+        currentValue += sender.titleLabel?.text ?? ""
+        
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        if (currentValue.isEmpty) {
+            resultLabel.text = "\(resultValue)".replacingOccurrences(of: ".", with: ",")
+        } else {
+            resultLabel.text = currentValue
+        }
+        
+        operatorLabel.text = operatorValue
+    }
+    
+    @IBAction func onOperatorClick(_ sender: UIButton) {
+        if (operatorValue.isEmpty) {
+            resultValue += currentValue.floatValue
+        }
+        
+        operatorValue = sender.titleLabel?.text ?? ""
+        
+        currentValue = ""
+        
+        updateLabel()
+    }
+    
+    @IBAction func onCalculate(_ sender: Any) {
+        switch operatorValue {
+            case "/":
+                resultValue /= currentValue.floatValue
+            case "+":
+                resultValue += currentValue.floatValue
+            case "-":
+                resultValue -= currentValue.floatValue
+            case "X":
+                resultValue *= currentValue.floatValue
+            default:
+                resultValue += currentValue.floatValue
+        }
+        
+        currentValue = ""
+        
+        updateLabel()
+    }
+    
+    @IBAction func onReset(_ sender: Any) {
+        currentValue = ""
+        resultValue = 0.0
+        operatorValue = ""
+        
+        updateLabel()
+    }
+    
 }
 
